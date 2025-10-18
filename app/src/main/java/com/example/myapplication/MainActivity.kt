b4fef89766b87a6bb93019870eb4a22d2f7b4fdc
maryamayman21 <mariam.ayman.ma28@gmail.com>
2025-10-19 01:15:58 +0300

    add directories for different build types for different credentials

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.data.provider.DataProvider
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(DataProvider.name)
                 }
             }
         }
